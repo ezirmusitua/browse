aefdd7ab9ab0e057f4d567f3a666ba97a6594dcb
ezirmusitua <jferroal@gmail.com>
2023-05-15 08:22:09 +0800

    Refactor: use img instead of next/img

--- packages/app/components/[dir]/components/mimes/Image.tsx
+++ packages/app/components/[dir]/components/mimes/Image.tsx
@@ -5,19 +5,50 @@ import { useContext } from "react";
 import Loading, { LoadingContext } from "../Loading";
 import { iMimeBaseProps } from "./interface";
 
+interface iImageProps {
+  use_next?: boolean;
+  alt?: string;
+  src: string;
+  onLoadingComplete: {
+    (): void;
+  };
+}
+
+function ImageRender({
+  use_next = false,
+  alt = "",
+  src,
+  onLoadingComplete,
+}: iImageProps) {
+  if (use_next) {
+    return (
+      <NextImage
+        className="object-contain"
+        src={src}
+        onLoadingComplete={onLoadingComplete}
+        alt={alt}
+        fill
+        quality={50}
+      ></NextImage>
+    );
+  }
+  return (
+    // eslint-disable-next-line
+    <img
+      alt={alt}
+      src={src}
+      className="h-full w-full object-contain"
+      onLoad={onLoadingComplete}
+    ></img>
+  );
+}
+
 function ImageViewer({ src, mime }: iMimeBaseProps) {
   const { hide: hideLoading } = useContext<{ hide: any }>(LoadingContext);
   if (!mime.startsWith("image")) return null;
   return (
     <div className="relative w-full h-full">
-      <NextImage
-        className="object-contain"
-        src={src}
-        onLoadingComplete={hideLoading}
-        alt=""
-        fill
-        quality={50}
-      ></NextImage>
+      <ImageRender src={src} onLoadingComplete={hideLoading}></ImageRender>
       <Loading></Loading>
     </div>
   );
